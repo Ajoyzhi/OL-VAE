d154af419fa8d168c6da880eaf30a7a43e641036
Ajoyzhi <303193532@qq.com>
2020-11-29 11:31:02 +0800

    finished processing with the KDD99 data,try to load

---
+++ dataset/KDD99/processed/KDD99_loader.py
@@ -0,0 +1,11 @@
+
+
+class KDD99_Loader():
+    self.train_normal_data = None
+    self.train_normal_label = None
+
+    self.test_data = None
+    self.test_label = None
+
+    self.train_normal_set = None
+    self.test_set = None
