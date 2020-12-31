2e56db6ffc39f95d417ea9e3a372b0583f730b6a
Ajoyzhi <303193532@qq.com>
2020-12-31 12:37:06 +0800

    upload VAE_Guass network structure

---
+++ network/VAE_Gauss.py
@@ -0,0 +1,65 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+class VAE_MNIST(nn.Module):
+    def __init__(self):
+        super(VAE_MNIST, self).__init__()
+
+        self.fc1 = nn.Linear(784, 400)
+        self.fc21 = nn.Linear(400, 20)
+        self.fc22 = nn.Linear(400, 20)
+        self.fc3 = nn.Linear(20, 400)
+        self.fc4 = nn.Linear(400, 784)
+
+    def encode(self, x):
+        h1 = F.relu(self.fc1(x))
+        return self.fc21(h1), self.fc22(h1)
+
+    def reparameterize(self, mu, logvar):
+        # Ajoy 求 e^(0.5*logvar) 相当于求var^0.5(方差)
+        std = torch.exp(0.5*logvar)
+        # Ajoy 生成与std大小一致的服从标准正态分布的数据 epsilon
+        eps = torch.randn_like(std)
+        return mu + eps*std
+
+    def decode(self, z):
+        h3 = F.relu(self.fc3(z))
+        return torch.sigmoid(self.fc4(h3))
+
+    def forward(self, x):
+        mu, logvar = self.encode(x.view(-1, 784))
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z), mu, logvar
+
+
+# 可以加入对输入特征数量的选择
+class VAE_KDD99(nn.Module):
+    def __init__(self):
+        super(VAE_KDD99, self).__init__()
+
+        self.fc1 = nn.Linear(15, 50)
+        self.fc21 = nn.Linear(50, 15)
+        self.fc22 = nn.Linear(50, 15)
+        self.fc3 = nn.Linear(15, 50)
+        self.fc4 = nn.Linear(50, 15)
+
+    def encode(self, x):
+        h1 = F.relu(self.fc1(x))
+        return self.fc21(h1), self.fc22(h1)
+
+    def reparameterize(self, mu, logvar):
+        # Ajoy 求 e^(0.5*logvar) 相当于求var^0.5(方差)
+        std = torch.exp(0.5*logvar)
+        # Ajoy 生成与std大小一致的服从标准正态分布的数据 epsilon
+        eps = torch.randn_like(std)
+        return mu + eps*std
+
+    def decode(self, z):
+        h3 = F.relu(self.fc3(z))
+        return torch.sigmoid(self.fc4(h3))
+
+    def forward(self, x):
+        mu, logvar = self.encode(x.view(-1, 15))
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z), mu, logvar
