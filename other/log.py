b1facc68d07a6649f8b44ea0cb5b873d64469f53
Ajoyzhi <303193532@qq.com>
2020-12-31 12:39:48 +0800

    upload log fuction and path file

---
+++ other/log.py
@@ -0,0 +1,28 @@
+import logging
+import time
+
+
+def init_log(filepath):
+    # 获取本地时间
+    real_time = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime(time.time()))
+    # 为你的log起个名字
+    logger = logging.getLogger(__name__)
+    # 设置输出等级，debug<info<warning等
+    logger.setLevel(level=logging.DEBUG)
+
+    # 将log输出到文件中
+    handler = logging.FileHandler(filepath + real_time + ".log")
+    # 设置等级，不设置默认用上面的输出等级
+    # handler.setLevel(logging.INFO)
+    # 设置格式 时间-输出内容
+    formatter = logging.Formatter('%(asctime)s - %(message)s')
+    # 装载格式
+    handler.setFormatter(formatter)
+    # 将log输出到控制台
+    console = logging.StreamHandler()
+    # console.setLevel(logging.DEBUG)
+
+    # 将整个两个添加到进logger中
+    logger.addHandler(handler)
+    logger.addHandler(console)
+    return logger
--- other/path.py
+++ other/path.py
@@ -1,4 +1,4 @@
-# 训练集的文件路径
+# kdd99训练集的文件路径
 Train_Src_Path = 'F:/pycharm/OL_VAE/dataset/KDD99/raw/kddcup.data_10_percent_corrected'
 # 数值化的训练数据文件路径
 Train_Des_Path = 'F:/pycharm/OL_VAE/dataset/KDD99/raw/kddcup.data_10_percent_corrected_number.cvs'
@@ -6,4 +6,5 @@ Train_Des_Path = 'F:/pycharm/OL_VAE/dataset/KDD99/raw/kddcup.data_10_percent_cor
 Test_Src_Path = 'F:/pycharm/OL_VAE/dataset/KDD99/raw/corrected'
 # 数值化的测试数据文件路径
 Test_Des_Path = 'F:/pycharm/OL_VAE/dataset/KDD99/raw/corrected_number.cvs'
-
+# KDD99 log输出位置
+Log_Path = 'F:/pycharm/OL_VAE/other/'
