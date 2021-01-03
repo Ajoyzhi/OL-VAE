9c7e32dec8d9c8633d259c92e0d77a4f2b5ecec8
Ajoyzhi <303193532@qq.com>
2021-01-03 21:44:57 +0800

    little changes

--- other/path.py
+++ other/path.py
@@ -7,4 +7,4 @@ Test_Src_Path = 'F:/pycharm/OL_VAE/dataset/KDD99/raw/corrected'
 # 数值化的测试数据文件路径
 Test_Des_Path = 'F:/pycharm/OL_VAE/dataset/KDD99/raw/corrected_number.cvs'
 # KDD99 log输出位置
-Log_Path = 'F:/pycharm/OL_VAE/other/'
+Log_Path = 'F:/pycharm/OL_VAE/other/log/'
