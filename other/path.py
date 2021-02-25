2fbffbd7ce1e3a10bf61eab7a7fbe3ff96c16ef4
Ajoyzhi <303193532@qq.com>
2021-02-25 10:08:35 +0800

    add main direction

--- other/path.py
+++ other/path.py
@@ -1,21 +1,21 @@
-# main文件所在目录为E:/pycharm/OL_VAE
+# main文件所在目录为E:/pycharm/OL_VAE/main
 # kdd99训练集的文件路径
-Train_Src_Path = './dataset/KDD99/raw/kddcup.data_10_percent_corrected'
+Train_Src_Path = '../dataset/KDD99/raw/kddcup.data_10_percent_corrected'
 # 数值化的训练数据文件路径
-Train_Number_Path = './dataset/KDD99/raw/kddcup.data_10_percent_corrected_number.cvs'
+Train_Number_Path = '../dataset/KDD99/raw/kddcup.data_10_percent_corrected_number.cvs'
 # 选择特征后的训练数据文件路径
-Train_Feature_Path =  './dataset/KDD99/raw/kddcup.data_10_percent_corrected_feature.cvs'
+Train_Feature_Path =  '../dataset/KDD99/raw/kddcup.data_10_percent_corrected_feature.cvs'
 # 按比例选择后的数据，标准化和归一化之后的训练数据（最终使用的训练数据）
-Train_Des_Path = './dataset/KDD99/raw/kddcup.data_10_percent_corrected_destination.cvs'
+Train_Des_Path = '../dataset/KDD99/raw/kddcup.data_10_percent_corrected_destination.cvs'
 
 # 测试集的文件路径
-Test_Src_Path = './dataset/KDD99/raw/corrected'
+Test_Src_Path = '../dataset/KDD99/raw/corrected'
 # 数值化的训练数据文件路径
-Test_Number_Path = './dataset/KDD99/raw/corrected_number.cvs'
+Test_Number_Path = '../dataset/KDD99/raw/corrected_number.cvs'
 # 选择特征后的训练数据文件路径
-Test_Feature_Path =  './dataset/KDD99/raw/corrected_feature.cvs'
+Test_Feature_Path =  '../dataset/KDD99/raw/corrected_feature.cvs'
 # 按比例选择后的数据，标准化和归一化之后的训练数据（最终使用的训练数据）
-Test_Des_Path = './dataset/KDD99/raw/corrected_destination.cvs'
+Test_Des_Path = '../dataset/KDD99/raw/corrected_destination.cvs'
 
 # KDD99 log输出位置
-Log_Path = './other/log/'
+Log_Path = '../other/log/'
