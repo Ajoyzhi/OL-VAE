9c4d875bae777885a3691743b995942e132bdbdd
Ajoyzhi <303193532@qq.com>
2021-03-02 08:49:44 +0800

    modify the plotting error

--- main/VAE_1D_comp.py
+++ main/VAE_1D_comp.py
@@ -8,7 +8,7 @@ from optim.VAE_1D_Online_trainer import OLVAE_1D_trainer
 import matplotlib.pyplot as plt
 
 # 生成训练数据：高斯分布 + (0,1)均匀分布噪声 100个
-x_train_temp = torch.ones(100, 1)
+x_train_temp = torch.ones(1000, 1)
 x_train_normal = torch.normal(x_train_temp, 2)# mu=1,std=2的离散高斯分布
 x_train_noise = torch.rand_like(x_train_normal)# (0,1)均匀分布
 x_train = x_train_normal + x_train_noise
@@ -23,7 +23,7 @@ train_loader = Data.DataLoader(
 )
 """
 # 生成测试数据：不同高斯分布 + (0,1)均匀分布噪声 300个
-x_test_temp = torch.ones(150)
+x_test_temp = torch.ones(150, 1)
 x_test_normal = torch.normal(x_test_temp, 2) # 正常数据
 y_normal = torch.zeros_like(x_test_normal)
 x_test_adnormal = torch.normal(2 * x_test_temp, 3) # 异常数据
@@ -57,7 +57,9 @@ VAE_Online_net = VAE_Online()
 vae_1D_trainer = VAE_1D_trainer(VAE_1D_net, train_loader)
 vae_Online_trainer = OLVAE_1D_trainer(VAE_1D_net, train_loader)
 vae_1D_trainer.train()
+vae_1D_trainer.test()
 vae_Online_trainer.train()
+vae_Online_trainer.test()
 
 # 获取数据，并画图
 epoch = vae_1D_trainer.epochs
@@ -74,26 +76,25 @@ ol_train_var = vae_Online_trainer.train_var
 ol_test_time = vae_Online_trainer.test_time
 
 # 画图函数
-def my_plot(loc: int, name: str, org_data, online_data, epoch, ):
+def my_plot(loc: int, name: str, org_data, online_data, epoch):
     my_plt = plt.subplot(loc)
-    line1 = my_plt.plot(org_data, range(1, epoch), color='b', linestyle='*')
-    line2 = my_plt.plot(online_data, range(1, epoch), color='r', linestyle='+')
+    line1 = my_plt.plot(range(1, epoch+1), org_data, 'b-')
+    line2 = my_plt.plot(range(1, epoch+1), online_data, 'r--')
     plt.xlabel('epoch')
     plt.ylabel(name)
     plt.title(name + 'of original VAE vs. online VAE')
-    my_plt.legend(line1, ('original VAE'))
-    my_plt.legend(line2, ('online VAE'))
-    my_plt.show()
+    my_plt.legend(line1, ('org VAE'))
+    my_plt.legend(line2, ('ol VAE'))
 
 plt.figure(figsize=(12, 6))
 # 画train_time的图
-my_plot(151, org_train_time,ol_train_time, epoch, 'train_time')
+my_plot(151, 'train_time', org_train_time, ol_train_time, epoch)
 # 画 train_loss的图
-my_plot(152, org_train_loss, ol_train_loss, epoch, 'train_loss')
+my_plot(152, 'train_loss', org_train_loss, ol_train_loss, epoch)
 # 画train_mu的图
-my_plot(153, org_train_mu, ol_train_mu, epoch, 'train_mu')
+my_plot(153, 'train_mu', org_train_mu, ol_train_mu, epoch)
 # 画train_var的图
-my_plot(154, org_train_var, ol_train_var, epoch, 'train_var')
+my_plot(154, 'train_var', org_train_var, ol_train_var, epoch)
 # 画test_time(一个值使用柱状图表示)
 test_time = [org_test_time, ol_test_time]
 x = ['original VAE', 'online VAE']
