58670f83c15c895686a1c3846bbc6a332e270422
Ajoyzhi <303193532@qq.com>
2021-03-31 19:44:57 +0800

    add test

--- main/test_AE.py
+++ main/test_AE.py
@@ -1,6 +1,6 @@
 from dataset.KDD99.processed.KDD99_loader import KDD99_Loader
-from optim.VAE_KDD99_trainer import VAE_Kdd99_trainer
-from network.VAE_KDD99 import VAE_KDD99
+from optim.AE_KDD99_trainer import AE_KDD99_trainer
+from network.AE_KDD99 import AE_KDD99
 from mytest.VAE_AE_test import metric
 
 # 测试kdd99的数值化处理
@@ -9,15 +9,16 @@ kdd99_train_loader = KDD99_Loader(ratio=0.01, isTrain=True, preprocess=False, lo
                                   batch_size=128, shuffle=False, num_workers=0).loader
 kdd99_test_loader = KDD99_Loader(ratio=0.001, isTrain=False, preprocess=False,loadData=True,
                                  batch_size=1, shuffle=False, num_workers=0).loader
+
 # 创建kdd99_VAE_Guass网络结构
-kdd99_vae = VAE_KDD99()
+kdd99_ae = AE_KDD99()
 # 创建trainer，并训练网络
-kdd99_vae_trainer = VAE_Kdd99_trainer(net=kdd99_vae, trainloader=kdd99_train_loader, testloader=kdd99_test_loader, epochs=50)
-kdd99_vae_trainer.train()
-kdd99_vae_trainer.get_normal_parm()
+kdd99_ae_trainer = AE_KDD99_trainer(net=kdd99_ae, trainloader=kdd99_train_loader, testloader=kdd99_test_loader, epoch=50, cluster_num=4)
+kdd99_ae_trainer.train()
+kdd99_ae_trainer.get_param()
 # 测试算法性能
-kdd99_vae_trainer.test()
-acc, pre, recall, FPR, MCC = metric(kdd99_vae_trainer.index_label_prediction)
+kdd99_ae_trainer.test()
+acc, pre, recall, FPR, MCC = metric(kdd99_ae_trainer.index_label_prediction)
 print("acc:", acc,
       "pre:", pre,
       "recall:", recall,
--- main/test_VAE_prob.py
+++ main/test_VAE_prob.py
@@ -1,23 +1,23 @@
 from dataset.KDD99.processed.KDD99_loader import KDD99_Loader
-from optim.VAE_KDD99_trainer import VAE_Kdd99_trainer
+from optim.VAE_prob_KDD99_trainer import VAE_prob_KDD99_trainer
 from network.VAE_KDD99 import VAE_KDD99
 from mytest.VAE_AE_test import metric
 
 # 测试kdd99的数值化处理
 # trainer data：97278； test_data：正常60593 异常223289
-kdd99_train_loader = KDD99_Loader(ratio=0.01, isTrain=True, preprocess=False, loadData=True,
+kdd99_train_loader = KDD99_Loader(ratio=0.01, isTrain=True, preprocess=False, loadData=False,
                                   batch_size=128, shuffle=False, num_workers=0).loader
-kdd99_test_loader = KDD99_Loader(ratio=0.001, isTrain=False, preprocess=False,loadData=True,
+kdd99_test_loader = KDD99_Loader(ratio=0.001, isTrain=False, preprocess=False,loadData=False,
                                  batch_size=1, shuffle=False, num_workers=0).loader
 # 创建kdd99_VAE_Guass网络结构
 kdd99_vae = VAE_KDD99()
 # 创建trainer，并训练网络
-kdd99_vae_trainer = VAE_Kdd99_trainer(net=kdd99_vae, trainloader=kdd99_train_loader, testloader=kdd99_test_loader, epochs=50)
-kdd99_vae_trainer.train()
-kdd99_vae_trainer.get_normal_parm()
+kdd99_prob_vae_trainer = VAE_prob_KDD99_trainer(net=kdd99_vae, trainloader=kdd99_train_loader, testloader=kdd99_test_loader, epoch=50, simple_num=10, alpha=1)
+kdd99_prob_vae_trainer.train()
+# kdd99_prob_vae_trainer.get_normal_parm()
 # 测试算法性能
-kdd99_vae_trainer.test()
-acc, pre, recall, FPR, MCC = metric(kdd99_vae_trainer.index_label_prediction)
+kdd99_prob_vae_trainer.test()
+acc, pre, recall, FPR, MCC = metric(kdd99_prob_vae_trainer.index_label_prediction)
 print("acc:", acc,
       "pre:", pre,
       "recall:", recall,
