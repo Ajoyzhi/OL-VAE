6f7bb990cf9ae31b994f464d30b1a850d3a66fcb
Ajoyzhi <303193532@qq.com>
2021-04-26 09:49:44 +0800

    modify the features of training data

--- dataset/SDN/processd/SDN_dataset.py
+++ dataset/SDN/processd/SDN_dataset.py
@@ -13,7 +13,9 @@ class sdnDataset(Dataset):
         id = np.loadtxt(self.src_path, dtype=str, delimiter=',', skiprows=0, usecols=(0, 1, 2, 3))
         self.id = id.tolist()
         # print("sdndataset_id:", self.id)
-        data = np.loadtxt(self.src_path, dtype=float, delimiter=',', skiprows=0, usecols=(4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16))
+        # data = np.loadtxt(self.src_path, dtype=float, delimiter=',', skiprows=0, usecols=(4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16))
+        # data = np.loadtxt(self.src_path, dtype=float, delimiter=',', skiprows=0, usecols=(4, 5, 6, 7, 8, 9, 10, 11, 16))
+        data = np.loadtxt(self.src_path, dtype=float, delimiter=',', skiprows=0, usecols=(4, 5, 6, 7, 8, 9, 10, 11))
         self.data = torch.Tensor(data)
         # print("sdn_dataset_data:", self.data)
 
